7cadaae2c202df4688c1c08d7b113b23a6c43828
BaldyLord <damian.jagiello@microsoft.wsei.edu.pl>
2023-04-04 17:32:13 +0200

    CalculateStatistics/CalculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,6 +10,22 @@ namespace GradeBook.GradeBooks
             this.Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5) throw new InvalidOperationException();
